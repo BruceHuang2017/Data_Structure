732815f688671111ac57995cced6607ad8a64079
BruceHib <huangyoupeng93@gmail.com>
2016-09-23 16:23:06 -0500

    Update bfs_and_dfs_code.py

--- bfs_and_dfs_code.py
+++ bfs_and_dfs_code.py
@@ -1,7 +1,3 @@
-import urllib2
-from collections import deque
-from bs4 import BeautifulSoup
-
 def getLinks(url, baseurl = "http://secon.utulsa.edu/cs2123/webtraverse/"):
     soup = BeautifulSoup(urllib2.urlopen(url).read(), "html5lib")
     urls = [baseurl + n['href'].encode('ascii', 'ignore') for n in soup.findAll('a')]
@@ -30,7 +26,7 @@ def generator_bfs(url, S = None):       # bfs algorithm generator
         if u in S: continue             # visited node, skip
         S.add(u)                        # set u as visited node
         g = getLinks(u)                 # get list of neighbors
-    	g = sorted(g, key=str.lower)	# sort by a, b, c, d ... with case-insensitive string comparation
+ #   	g = sorted(g, key=str.lower)	# sort by a, b, c, d ... with case-insensitive string comparation
     	Q.extend(g)           		# find all neighbors with u
         yield u                         # u is done, processed
 
@@ -48,20 +44,21 @@ def bfs_parents(G, s):
     return P
 
 def find_shortest_path_printer(url1,url2):
-    node = list(generator_bfs(url1))
+    url = "http://secon.utulsa.edu/cs2123/webtraverse/index.html"
+    node = list(generator_bfs(url))
     G = dict((u, getLinks(u)) for u in node)  	# find undirected graph
-    try:    
+    try:
         P = bfs_parents(G, url1)			# bfs parents dictionary start with url1
         path = [url2]	                  	# use bfs parents dictionary P to find path between A and B
         while P[url2] != url1:		# when url1 is not next to url2
             if P[url2] is None:             # stop when url2 is root
-                print 'There is no way the first url can reach the second url.'
+                print 'There is no way the first url can reach the second one.'
                 break
             path.append(P[url2])            # add bfs parent of url2 to path, go one step
             url2 = P[url2]                  # based on the new node, loop till reach target
         path.append(P[url2])                # add initial value
         path.reverse()                      # reverse path to start from url1
-    	print path 			# instead using return path, use print path ????????????????
+    	yield path 			# instead using return path, use print path ????????????????
     except KeyError, e:
 	pass				    #'There is no way the first url can reach the second url.'
     try:
@@ -69,42 +66,40 @@ def find_shortest_path_printer(url1,url2):
         path = [url1]                           # use bfs parents dictionary P to find path between A and B
         while P[url1] != url2:          # when url1 is not next to url2
             if P[url1] is None:             # stop when url2 is root
-                print 'There is no way the first url can reach the second url.'
+                print 'There is no way the second url can reach the first one.'
                 break
             path.append(P[url1])            # add bfs parent of url2 to path, go one step
             url2 = P[url1]                  # based on the new node, loop till reach target
         path.append(P[url1])                # add initial value
-        path.reverse()                      # reverse path to start from url1
-	print path
+        path.reverse()                     # reverse path to start from url1
+	yield path
     except KeyError, e:
 	pass
 
 def find_shortest_path(url1,url2):
-    path = find_shortest_path_printer(url1,url2)
-    if path == None:
+    path = list(find_shortest_path_printer(url1,url2))
+    if path == []:
         print 'Path not Found Between',url1,'and',url2     #'There is no way second url can reach the first url.'
     else:
-	print path
+	print path[0]
 
 def find_max_depth(start_url):
     node = list(generator_bfs(start_url))
     G = dict((u, getLinks(u)) for u in node) 				# find undirected graph
-    P = bfs_parents(G, starturl)
-    l = {}
-    target_url = starturl
+    P = bfs_parents(G, start_url)
+    target_url = ''
     maxl = 0
-    for u in P:
-	for v in P[u]:
-	    r = find_shortest_path_printer(start_url, v)
-	    if r == None: 
-		l[v] = 'None'
-		continue
-	    else:
-		l[v] = len(r)
-		if maxl < len(r):
-		    maxl = len(r)
+    counter = 0
+    for v in P.keys():
+	if v != None and v != start_url:
+	    r = list(find_shortest_path_printer(start_url, v))
+	    print 'r is:', r
+	    if r != []:
+		if maxl < len(r[0]):
+		    maxl = len(r[0])
 		    target_url = v
-    return target_url
+		    print 'Newest target url is:', target_url, ', I already change it ',counter,'times.'
+    print target_url
 
 if __name__=="__main__":
     starturl = "http://secon.utulsa.edu/cs2123/webtraverse/index.html"
